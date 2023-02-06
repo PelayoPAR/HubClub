75e236dc158250d5a6617ef72dde8183b7a524a1
Kira <kira.soba@gmx.de>
2023-02-06 19:24:03 +0100

    delete test comment

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ function App() {
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/ironhackers/:name" element={<SingleIronhacker />}></Route>
       </Routes>
-      {/* test */}
     </div>
   );
 }
